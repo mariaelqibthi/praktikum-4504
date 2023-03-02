8357affa1e1a1644eb22ef5d3945f2bf92996704
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-03-02 14:13:14 +0700

    initial commit task 1

--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -2,10 +2,19 @@ package org.d3if3137.praktikum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nextButton: Button = findViewById(R.id.nextButton)
+        nextButton.setOnClickListener{ showNext() }
+    }
+
+    private fun showNext() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
