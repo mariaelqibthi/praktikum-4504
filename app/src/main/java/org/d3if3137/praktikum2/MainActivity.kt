30f813a70b46beb7f705664e036d344b4cae08a1
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-03-09 00:14:34 +0700

    initial commit task 1.2 final bmi done

--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -19,6 +19,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun hitungBmi() {
-        Log.d("MainActivity", "Tombol diklik!")
+        val berat = binding.beratEditText.text.toString().toFloat()
+        val tinggi = binding.tinggiEditText.text.toString().toFloat() / 100
+        val bmi = berat / (tinggi * tinggi)
+
+        val selectedId = binding.radioGroup.checkedRadioButtonId
+        val isMale = selectedId == R.id.priaRadioButton
+        val kategori = getKategori(bmi, isMale)
+
+        binding.bmiTextView.text = getString(R.string.bmi_x, bmi)
+        binding.kategoriTextView.text = getString(R.string.kategori_x, kategori)
+
+    }
+
+    private fun getKategori(bmi: Float, isMale: Boolean): String {
+        val stringRes = if (isMale){
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }else {
+            when {
+                bmi < 20.5 -> R.string.kurus
+                bmi >= 27.0 -> R.string.gemuk
+                else -> R.string.ideal
+            }
+        }
+        return getString(stringRes)
     }
 }
