7a2d68efc3363cd0939d72bb716243075f297427
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-03-08 23:46:22 +0700

    initial commit task 1.1 view binding

--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -2,10 +2,23 @@ package org.d3if3137.praktikum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import org.d3if3137.praktikum2.databinding.ActivityMainBinding
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener{ hitungBmi()}
+    }
+
+    private fun hitungBmi() {
+        Log.d("MainActivity", "Tombol diklik!")
     }
 }
