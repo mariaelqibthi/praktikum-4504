011348c7b2f055f3a9bff108b6612dc0fbb970d1
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-03-23 21:29:06 +0700

    initial commit task 1.2 Menambahkan view binding

--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -3,11 +3,15 @@ package org.d3if3137.praktikum2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import org.d3if3137.praktikum2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         Log.d("MainActivity", "Jumlah data: " + getData().size)
     }
