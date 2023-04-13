b415322a65fcbdd47a3508dcd1d4ef5c329c6a99
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-04-13 20:42:57 +0700

    initial commit membuat fragment saran

---
+++ app/src/main/java/org/d3if3137/hitungbmi/ui/SaranFragment.kt
@@ -0,0 +1,46 @@
+package org.d3if3137.hitungbmi.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.mariaelqibthi.hitungbmi.model.KategoriBmi
+import org.d3if3137.praktikum2.R
+import org.d3if3137.praktikum2.databinding.FragmentHitungBinding
+import org.d3if3137.praktikum2.databinding.FragmentSaranBinding
+import java.util.zip.Inflater
+
+class SaranFragment : Fragment() {
+    private lateinit var binding: FragmentSaranBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentSaranBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    private fun updateUI(kategori: KategoriBmi) {
+        val actionBar = (requireActivity() as AppCompatActivity).supportActionBar
+        when (kategori) {
+            KategoriBmi.KURUS -> {
+                actionBar?.title = getString(R.string.judul_kurus)
+                binding.imageView.setImageResource(R.drawable.kurus)
+                binding.textView.text = getString(R.string.saran_kurus)
+            }
+            KategoriBmi.IDEAL -> {
+                actionBar?.title = getString(R.string.judul_ideal)
+                binding.imageView.setImageResource(R.drawable.ideal)
+                binding.textView.text = getString(R.string.saran_ideal)
+            }
+            KategoriBmi.GEMUK -> {
+                actionBar?.title = getString(R.string.judul_gemuk)
+                binding.imageView.setImageResource(R.drawable.gemuk)
+                binding.textView.text = getString(R.string.saran_gemuk)
+            }
+        }
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        updateUI(KategoriBmi.KURUS)
+    }
+}
--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -2,7 +2,6 @@ package org.d3if3137.praktikum2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import org.d3if3137.praktikum2.databinding.ActivityMainBinding
 import androidx.lifecycle.ViewModelProvider
 import com.mariaelqibthi.hitungbmi.model.HasilBmi
 import com.mariaelqibthi.hitungbmi.model.KategoriBmi
@@ -10,67 +9,9 @@ import org.d3if3137.hitungbmi.ui.MainViewModel
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityMainBinding
-    private val viewModel: MainViewModel by lazy {
-        ViewModelProvider(this)[MainViewModel::class.java]
-    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-        binding.button.setOnClickListener{ hitungBmi()}
-        viewModel.getHasilBmi().observe(this, {showResult(it)})
-    }
-
-    private fun hitungBmi() {
-        val berat = binding.beratBadanInp.text.toString().toFloat()
-        val tinggi = binding.tinggiBadanInp.text.toString().toFloat()
-        val selectedId = binding.radioGroup.checkedRadioButtonId
-        viewModel.hitungBmi(
-            berat.toFloat(),
-            tinggi.toFloat(),
-            selectedId == R.id.priaRadioButton
-        )
-    }
-
-    private fun getKategori(bmi: Float, isMale: Boolean): KategoriBmi {
-        val kategori = if (isMale){
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }else {
-            when {
-                bmi < 20.5 -> KategoriBmi.KURUS
-                bmi >= 27.0 -> KategoriBmi.GEMUK
-                else -> KategoriBmi.IDEAL
-            }
-        }
-        return kategori
-    }
-
-    private fun getKategoriLabel(kategori: KategoriBmi): String {
-        val stringRes = when (kategori) {
-            KategoriBmi.KURUS -> R.string.kurus
-            KategoriBmi.IDEAL -> R.string.ideal
-            KategoriBmi.GEMUK -> R.string.gemuk
-        }
-    return getString(stringRes)
-    }
-
-    private fun hitungBmi(berat: Float, tinggi : Float, isMale: Boolean): HasilBmi {
-        val tinggiCm = tinggi / 100
-        val bmi = berat / (tinggiCm * tinggiCm)
-        val kategori = getKategori(bmi, isMale)
-        return HasilBmi(bmi, kategori)
-    }
-
-    private fun showResult(result: HasilBmi?){
-        if (result == null) return
-        binding.bmiTextView.text = getString(R.string.bmi_x, result.bmi)
-        binding.kategoriTextView.text = getString(R.string.kategori_x, getKategoriLabel(result.kategori))
     }
 }
