79d8b4377ced01c8027b2551f19b5c72c24da002
Maria El Qibthiya Az Zahra <mariaelqibthi@gmail.com>
2023-06-07 00:36:18 +0700

    3.1.Mempelajari daur hidup Activity

--- app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
+++ app/src/main/java/org/d3if3137/praktikum2/MainActivity.kt
@@ -3,6 +3,7 @@ package org.d3if3137.praktikum2
 //import android.os.Build.VERSION_CODES.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if3137.praktikum2.main.MainAdapter
@@ -12,5 +13,29 @@ class MainActivity : AppCompatActivity() { //mod4
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", "onCreate dijalankan")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart dijalankan")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("MainActivity", "onResume dijalankan")
+    }
+    override fun onPause() {
+        Log.i("MainActivity", "onPause dijalankan")
+        super.onPause()
+    }
+    override fun onStop() {
+        Log.i("MainActivity", "onStop dijalankan")
+        super.onStop()
+    }
+    override fun onDestroy() {
+        Log.i("MainActivity", "onDestroy dijalankan")
+        super.onDestroy()
     }
 }
